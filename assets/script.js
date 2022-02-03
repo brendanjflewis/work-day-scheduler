c8596945a65d9488d97c2af5c613a0e226f40432
brendanjlewis <brendan.j.f.lewis@gmail.com>
2022-02-03 03:01:10 -0500

    added rest of workday, attempt to get relative time working

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,39 @@
-var today = moment().format("MMMM Do YYYY, h:mm:ss a");
-
+// shows current date (added day of the week and removed seconds)
+var today = moment().format("dddd MMMM Do YYYY, h:mma");
 $("#currentDay").text(today);
 
 
 
+$(document).ready(function() {
+    // click function to store time with save button
+    saveBtn = $(".saveBtn").on("click", function() {
+        var time = $(this).siblings(".hour").text();
+        var plan = $(this).siblings(".plan").val();
+        console.log(this);
+
+        localStorage.setItem(time, plan);
+    })
+
+    function hourTracker() {
+        var currentTime = moment().hour();
+        
+        timeBlock = $(".time-block").each(function () {
+            var hour = parseInt($(this).attr("id"));
+
+
+            if (currentTime > hour) {
+                $(this).addClass("past");
+            } else if (currentTime === hour) {
+                $(this).addClass("present");
+            } else {
+                $(this).addClass("fuuuuutuuurrrre");
+            }
+
+            console.log(this);
+        })
+    }
+    hourTracker();
+})
 // $(document).ready(function() {
 //     var currentDate = momenent().format('MMM Do YY');
 //     $("#currentDay").html(currentDate);
