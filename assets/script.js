2fd9546d43c4e9b6d3f57a1310208aac714a8f81
brendanjlewis <brendan.j.f.lewis@gmail.com>
2022-02-03 02:05:52 -0500

    testing functionality of momentjs

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
-var todayDate = moment().format('dddd, MMM YY');
+var today = moment().format("MMMM Do YYYY, h:mm:ss a");
+
+$("#currentDay").text(today);
+
+
+
+// $(document).ready(function() {
+//     var currentDate = momenent().format('MMM Do YY');
+//     $("#currentDay").html(currentDate);
+//     $(".saveBtn").on("click", function () {
+        
+//     })
+// })
